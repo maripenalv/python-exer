bfb20a070607a4c53967cbe4dd5e1e3071eb0f4b
Mariana Penalva <marianapenalva07@gmail.com>
2024-08-30 10:11:39 -0300

    exer_modu_32.py

--- pythonProject/diretorio2/exer_modu_32.py
+++ pythonProject/diretorio2/exer_modu_32.py
@@ -1,9 +1,9 @@
 import moeda
 
 nmr= float(input("Digite um preço:"))
-print(f"Aumentando 10% de {nmr} temos {moeda.moeda(moeda.aumentar(nmr))}") #Primeiro importamos do módulo "moeda" a
-print(f"Dimunindo 10% de {nmr} temos {moeda.moeda(moeda.diminuir(nmr))}")  #função "moeda" que inclui a sigla "RS", o número
-print(f"O dobro do número {moeda.moeda(nmr)} é {moeda.moeda(moeda.dobro(nmr))}") #em si e a formatação do ponto pra vírgula.
-print(f"A metade do número {nmr} é {moeda.moeda(moeda.metade(nmr))}")            #Em seguida, aplicaremos essa formatação de
-                                                                           #sigla e vírgula ao número que sofrerá uma das
-                                                                           #operações matemáticas.
+print(f"Aumentando 10% de {nmr} temos {moeda.moeda(moeda.aumentar(nmr))}") 
+print(f"Dimunindo 10% de {nmr} temos {moeda.moeda(moeda.diminuir(nmr))}")  
+print(f"O dobro do número {moeda.moeda(nmr)} é {moeda.moeda(moeda.dobro(nmr))}")
+print(f"A metade do número {nmr} é {moeda.moeda(moeda.metade(nmr))}")            
+                                                                           
+                                                                           
