1bce26649651e75bf952c60d8beadb298f76c90e
Mariana Penalva <marianapenalva07@gmail.com>
2024-08-30 10:35:17 -0300

    Update exer29.py

--- pythonProject/exer29.py
+++ pythonProject/exer29.py
@@ -9,10 +9,10 @@ if jogador == "":
     jogador = "*campo desconhecido*"
 
 
-if gols.isdigit(): #primeiro verificamos se o input que "goals" recebe é composto por algum número. Caso sim, o input de "goals"
-    gols = int(gols) #(perceba que ele não recebeu nenhum tipo primitivo anteriormente) será convertido pra um número inteiro
+if gols.isdigit(): 
+    gols = int(gols) 
 else:
-    gols = "*campo desconhecido*" #se "goals" não recebeu nenhum dígito, ele será igual a "*campo desconhecido*"
+    gols = "*campo desconhecido*" 
 
 
 ficha(jogador, gols)
