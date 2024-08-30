fc9d2d291dc73d1378c3a4b5178df6aa8ed9966c
Mariana Penalva <marianapenalva07@gmail.com>
2024-08-30 10:18:47 -0300

    Update exer03.py

--- pythonProject/exer03.py
+++ pythonProject/exer03.py
@@ -1,7 +1,5 @@
 import random
 
-tupla = random.sample(range(1, 101), 5)   #a função "random.sample" escolhe aleatoriamente números dentro de um certo
-print(f"Os números sorteados foram: {tupla}") #intervalo(nesse caso entre 1 e 100) e pede a quantidade de nmrs que devem
-                                             #ser escolhidos(nesse caso, 5)
-
+tupla = random.sample(range(1, 101), 5)   
+print(f"Os números sorteados foram: {tupla}") 
 print(f"O maior valor da tupla foi {max(tupla)} e o menor, {min(tupla)}")
