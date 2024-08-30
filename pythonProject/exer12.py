050dfc330a51026f4b942600c196e467773fb059
Mariana Penalva <marianapenalva07@gmail.com>
2024-08-30 10:27:21 -0300

    Update exer12.py

--- pythonProject/exer12.py
+++ pythonProject/exer12.py
@@ -1,16 +1,16 @@
 lista = []
 escolha = input("Escreva uma expressão matemática com parenteses: ")
 
-for caractere in escolha:   #vai percorrer cada caractere escrito
-    if caractere == '(':    #se o caracter atual a ser analisado é "(" entao será adicionado à lista
+for caractere in escolha:   
+    if caractere == '(':    
         lista.append(caractere)
-    elif caractere == ')':  #se o caractere atual for ')' será feita uma verificação.
-        if not lista:       #se a lista estiver vazia, isso significa que encontramos um ')' sem seu par correspondente '('.
-            print("Os parênteses estão incorretamente posicionados.") #logo, os parenteses estão incorretamente posicionados
+    elif caractere == ')': 
+        if not lista:       
+            print("Os parênteses estão incorretamente posicionados.") 
             break
-        lista.pop()  #assim, removeremos o  '(' correspondente da lista
+        lista.pop() 
 
-if not lista:  #após percorrer toda a expressão, verifica-se se a lista, usada como uma pilha para rastrear os parênteses de abertura, está vazia. Se estiver, significa que todos os parênteses de abertura encontraram seus pares de fechamento. Durante a iteração, parênteses de abertura são adicionados à pilha e, quando encontramos um parêntese de fechamento, retiramos o parêntese de abertura correspondente da pilha, se houver. Isso garante que os parênteses estejam balanceados.
+if not lista:  
     print("Os parênteses estão corretamente posicionados.")
 else:
     print("Os parênteses estão incorretamente posicionados.")
