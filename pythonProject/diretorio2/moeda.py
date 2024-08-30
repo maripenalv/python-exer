800ba2a0f946e437e5a41410957aacf296d9648b
Mariana Penalva <marianapenalva07@gmail.com>
2024-08-30 10:12:38 -0300

    Update moeda.py

--- pythonProject/diretorio2/moeda.py
+++ pythonProject/diretorio2/moeda.py
@@ -11,8 +11,6 @@ def metade(preco):
 	conta= preco//2
 	return conta
 
-def moeda(preco=0, sigla= "RS"):                           #criaremos dois parametros: "preco" que já existe e um novo, "moeda2"
-     return f"{sigla}{preco}".replace(".",",") #caso o valor de preço não seja informado, será igual a "0", caso o
-                                                            #valor de moeda2 não for informado, receberá "RS"
-                                                            #Por fim, retornaremos a sigla da moeda seguido do preço e pedimos
-	                                                        #para que o programa mudasse os pontos(do padrão americano) pra vírgula(do brasileiro)
+def moeda(preco=0, sigla= "RS"):                          
+     return f"{sigla}{preco}".replace(".",",") 
+                                                            
