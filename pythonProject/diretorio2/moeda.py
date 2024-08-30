8c60d4b7f2d91fa402ffeddded6db08348c86b28
Mariana Penalva <marianapenalva07@gmail.com>
2024-08-30 10:11:55 -0300

    Update moeda.py

--- pythonProject/diretorio2/moeda.py
+++ pythonProject/diretorio2/moeda.py
@@ -1,9 +1,6 @@
 def aumentar(preco):
 	conta= (preco*0.10)+preco
-	return conta               #usamos o "return" pois ele permite que ele seja enviado pra qualquer lugar onde a função seja
-                               #chamada, armazenando-o. em o return, a função executaria seus cálculos, mas não devolveria nenhum
-	                           #valor ao ponto onde foi chamada. Como resultado, o valor calculado dentro da função não estaria
-	                           #disponível fora dela, e qualquer tentativa de usar o valor retornado resultaria em "None"
+	return conta               
 def diminuir(preco):
 	conta= preco-(preco*0.10)
 	return conta
