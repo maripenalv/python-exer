934fc269bace7040acb5399960f3beaca676d0ab
Mariana Penalva <marianapenalva07@gmail.com>
2024-08-30 10:19:17 -0300

    Update exer04.py

--- pythonProject/exer04.py
+++ pythonProject/exer04.py
@@ -5,15 +5,15 @@ nmr4= int(input("Digite o quarto número: "))
 
 tupla = (nmr1, nmr2, nmr3, nmr4)
 print(f"Os números digitados foram: {tupla}")
-print(f"O número 9 aparece {tupla.count(9)} vezes") #usamos "count" para contar as ocorrências de um elemento específico
-print(f"A posição do primeiro número 3 foi {tupla.index(3) +1}") #o index automaticamente encontra apenas a posiçao do primeiro número "tal" digitado
+print(f"O número 9 aparece {tupla.count(9)} vezes") 
+print(f"A posição do primeiro número 3 foi {tupla.index(3) +1}") 
 
-for numero in tupla:                            #podemos descobrir os números pares assim, porém eles aparecerão
-    if numero % 2 ==0:                          #por vez
+for numero in tupla:                            
+    if numero % 2 ==0:                          
         print(f"O número {numero} é par")
 
-lista = []                                      #dessa forma utilizamos uma lista, funcionará assim:
-for numero in tupla:                            #criaremos um loop "for" que percorrerá cada elemento da tupla, em seguida, por
-    if numero % 2 == 0:                         #meio do "if", os números que foram constatados que são pares serão anexados à uma
-        lista.append(numero)                    #lista, logo, ao printá-la, printaremos de vez todos os números pares contidos nela
+lista = []                                      
+for numero in tupla:                            
+    if numero % 2 == 0:                         
+        lista.append(numero)                    
 print(f"Os números pares são {lista}")
