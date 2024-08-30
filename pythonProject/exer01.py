3e26a179175b1b8106f3155a9d346ef2b9f55d44
Mariana Penalva <marianapenalva07@gmail.com>
2024-08-30 10:15:25 -0300

    Update exer01.py

--- pythonProject/exer01.py
+++ pythonProject/exer01.py
@@ -1,9 +1,7 @@
-from numero_por_extenso import numero_por_extenso #vamos utilizar essa biblioteca para tornar os nmrs extensos
+from numero_por_extenso import numero_por_extenso 
 
-tupla = tuple(range(21))                 #se utilizarmos somente o "range(21)", ao printar, o resultado não daria certo
-                                         #pois o range só printa um número por vez, por isso o uso de um loop se faz necessário.
-                                         #Para burlar isso, adicionamos "tuple" para que o range funcione como uma tupla, ou seja,
-                                         #para que todos os números de 0 a 20 sejam printados
+tupla = tuple(range(21))                 
+
 while True:
     escolha = int(input("Escolha um número de 0 a 20: "))
     if escolha not in tupla:
