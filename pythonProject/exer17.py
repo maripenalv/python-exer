260e89b44601fe03ab0ae170278026bb82dd9f7f
Mariana Penalva <marianapenalva07@gmail.com>
2024-08-30 10:29:27 -0300

    Update exer17.py

--- pythonProject/exer17.py
+++ pythonProject/exer17.py
@@ -6,5 +6,5 @@ contador = 1
 
 while contador <= quant:
     contador += 1
-    aposta.append(random.sample(range(1, 61), 6)) #pelo ".sample" e "range" geraremos 6 números que variam de 1 a 60
+    aposta.append(random.sample(range(1, 61), 6)) 
 print(f"Os números sorteados foram: {aposta}")
